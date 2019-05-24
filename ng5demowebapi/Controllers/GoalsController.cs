7b13e884dc35a2da6fbc917e625afe43189f26ce
saket adhav <saket.adhav@synerzip.com>
2019-05-24 11:59:10 +0530

    First commit

---
+++ ng5demowebapi/Controllers/GoalsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace ng5demowebapi.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class GoalsController : ApiController
+    {
+        private static List<string> goalList = new List<string>();
+
+        [HttpGet]
+        public List<string> Get()
+        {
+            return goalList;
+        }
+
+        [HttpPost]
+        public IHttpActionResult Post(Goal goal)
+        {
+            goalList.Add(goal.GoalText);
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Delete(string goaltext)
+        {
+            goalList.Remove(goaltext);
+            return Ok();
+        }
+
+        public class Goal
+        {
+            public int Id { get; set; }
+            public string GoalText { get; set; }
+        }
+    }
+}
---
+++ ng5demowebapi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ng5demowebapi.WebApiApplication" Language="C#" %>
